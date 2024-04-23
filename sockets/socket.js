56502631dd81ce03404771a7906266e19c08ad36
Jose Gregorio Aguilar D'Vicente <jgglobal12@gmail.com>
2024-04-23 12:46:00 -0400

    Update socket.js

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,6 +1,8 @@
 import { Server } from 'socket.io';
 
 export const connectSocket = (server, whitelist) => {
+	console.log(whitelist)
+	
 	const io = new Server(server, {
 		pingTimeout: 60000,
 		cors: {
